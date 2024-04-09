24310effb40a222237d11687f5367fa42dbc0a76
Dipunj Gupta <zero@dipunj.com>
2024-04-09 14:51:42 -0400

    responds with 200 OK

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
+use std::io::Write;
 use std::net::TcpListener;
+
+fn ok_response(mut stream: std::net::TcpStream) {
+    stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap()
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(s) => {
+                ok_response(s);
             }
             Err(e) => {
                 println!("error: {}", e);
